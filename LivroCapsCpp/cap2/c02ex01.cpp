3cb7f5f1b8ee7b0a744d725010f614db6f645398
Abraão Garcia <161896847+AbrGarcia01@users.noreply.github.com>
2026-05-11 16:51:46 -0300

    Create c02ex01.cpp
    Codigo padrão de apresentação

---
+++ LivroCapsCpp/cap2/c02ex01.cpp
@@ -0,0 +1,13 @@
+//c02ex01.cpp
+#include <iostream> 
+
+int main(void)
+{
+
+std::cout << "Ola,mundo!" << std::edl;
+
+std::cout << "Tecle <Enter> para encerrar...";
+std::cin.get();
+
+return 0;
+}
