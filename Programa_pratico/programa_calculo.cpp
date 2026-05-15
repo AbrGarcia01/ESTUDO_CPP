475ca5882507517d7baa7ac0f4ded1c29fcf1dae
Abraão Garcia <161896847+AbrGarcia01@users.noreply.github.com>
2026-05-15 16:59:05 -0300

    Create programa_calculo.cpp

---
+++ Programa_pratico/programa_calculo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main(void)
+{
+    int Valor1, Valor2;
+    
+    cout << "Entre o primeiro valor: "; cin >> Valor1;
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    cout << "Entre o segundo valor: "; cin >> Valor2;
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    
+    do{
+        cout << "Insira qual opcao de calculo gostaria de seguir? "   
+       
+        
+    }while(opcao = 5);
+}
