8576137387fbeaf8d692b6f6940b472d81534f3f
Abraão Garcia <161896847+AbrGarcia01@users.noreply.github.com>
2026-05-18 17:02:13 -0300

    Create programa01.cpp

---
+++ Programa_pratico/pratico cap1/programa01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/*
+  O programa deve ler: Nome do funcionario, Salario Base, Quantidade de horas extras.
+  Calcular o das horas extras, equivalente a 25. Calcular o salario bruto e o desconto do inss.
+*/ 
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main(void)
+{
+  cout << "BEM-VINDO AO PROGRAMA01.CPP" << endl << endl;
+
+  
+  
+}
