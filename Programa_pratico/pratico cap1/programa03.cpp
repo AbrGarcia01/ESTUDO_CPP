dbdee2e630994ba5628c44dadadba7f2274b4348
Abraão Garcia <161896847+AbrGarcia01@users.noreply.github.com>
2026-05-19 17:03:42 -0300

    Add menu function for banking application

---
+++ Programa_pratico/pratico cap1/programa03.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void Menu()
+{
+  cout << "=========================" << endl;
+  cout << "====== MENU BANCO =======" << endl;
+  cout << "=========================" << endl;
+  cout << "======[1]DEPOSITAR========" << endl; 
+  cout << "========[2]SACAR==========" << endl; 
+  cout << "======[3]VER SALDO========" << endl;
+  cout << "========[4]SAIR===========" << endl; 
+}
