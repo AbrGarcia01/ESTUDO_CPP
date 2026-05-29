019e554b619d312ee1493de2f2487a9d1e0cd4ee
Abraão Garcia <161896847+AbrGarcia01@users.noreply.github.com>
2026-05-29 08:00:07 -0300

    Create programa08.cpp

---
+++ Programa_pratico/pratico cap1/programa08.cpp
@@ -0,0 +1,13 @@
+// Criar uma matriz bool que leia false para string e true para int or float
+
+#include <iostream>
+#include <string>
+#include <array>
+
+using namespace std;
+
+int main(void)
+{
+
+    
+}
